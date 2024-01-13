2509bd8b7e78ebeb0baaeadd9c7b70bd62ecfa85
0roc4n <76811555+0roc4n@users.noreply.github.com>
2024-01-13 17:37:06 +0800

    update create gitignore

--- app/Http/Controllers/AddMemberController.php
+++ app/Http/Controllers/AddMemberController.php
@@ -12,18 +12,18 @@ class AddMemberController extends Controller
     }
    
 
-    public function store(Request $request){
-        Member::create([
-            'name' => $request->input('name'),
-            'email' => $request->input('email'),
-            'gender' => $request->input('gender'),
-            'contact_number' => $request->input('contact_number'),
-            'birthday' => $request->input('birthday'),
-            'purok' => $request->input('purok'),
-            'youth_classification' => $request->input('youth_classification'),
-            'card_id' => $request->input('card_id'),
-            'age' => $request->input('age')
-        ]);
-        return redirect('/members');
-    }
+    // public function store(Request $request){
+    //     Member::create([
+    //         'name' => $request->input('name'),
+    //         'email' => $request->input('email'),
+    //         'gender' => $request->input('gender'),
+    //         'contact_number' => $request->input('contact_number'),
+    //         'birthday' => $request->input('birthday'),
+    //         'purok' => $request->input('purok'),
+    //         'youth_classification' => $request->input('youth_classification'),
+    //         'card_id' => $request->input('card_id'),
+    //         'age' => $request->input('age')
+    //     ]);
+    //     return redirect('/members');
+    // }
 }
